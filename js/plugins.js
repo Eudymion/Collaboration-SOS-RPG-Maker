0769802f23e84a7937b686cc49964bef85499c2e
Hinkay <87575916+Hinkay@users.noreply.github.com>
2021-07-25 17:10:42 +0200

    Cinématique départ
    cinématique départ + discours chat

--- js/plugins.js
+++ js/plugins.js
@@ -4,5 +4,6 @@ var $plugins =
 [
 {"name":"Community_Basic","status":true,"description":"Basic plugin for manipulating important parameters.","parameters":{"cacheLimit":"20","screenWidth":"816","screenHeight":"624","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}},
 {"name":"MadeWithMv","status":false,"description":"Show a Splash Screen \"Made with MV\" and/or a Custom Splash Screen before going to main screen.","parameters":{"Show Made With MV":"true","Made with MV Image":"MadeWithMv","Show Custom Splash":"false","Custom Image":"","Fade Out Time":"120","Fade In Time":"120","Wait Time":"160"}},
-{"name":"SkipTitle","status":false,"description":"v1.00 Skips Title Screen\r\n<SilverSkipTitle>","parameters":{"Skip Title Screen Entirely":"true","Skip If Savefile Present":"true","FadeOut Title":"false"}}
+{"name":"SkipTitle","status":false,"description":"v1.00 Skips Title Screen\r\n<SilverSkipTitle>","parameters":{"Skip Title Screen Entirely":"true","Skip If Savefile Present":"true","FadeOut Title":"false"}},
+{"name":"SRD_CameraCore","status":true,"description":"Allows developers to preform various camera motions including focusing and zooming.","parameters":{"Default Zoom":"1","Zoom Pictures?":"true","Margin Formula":"(Graphics.width / scale) - Graphics.width","Fix Black Lines":"false"}}
 ];
